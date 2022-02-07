c95597905d7140551d1ee32277f4d5cb1ee4222b
VivaaindreanNg <123ulugubang@gmail.com>
2022-02-07 08:06:34 +0800

    Updated test cases.

--- tutorialProject1/api/tests.py
+++ tutorialProject1/api/tests.py
@@ -1,20 +1,17 @@
 # Create your tests here.
 from api.models import Task
-from django.test import Client, TestCase
-from django.test.utils import setup_test_environment, teardown_test_environment
 from model_mommy import mommy
+from rest_framework.test import APITestCase
 
 
-class TestView(TestCase):
+class TestView(APITestCase):
     def setUp(self) -> None:
-        teardown_test_environment()
-        setup_test_environment()
-        self.client = Client()
         self.task1 = mommy.prepare(Task, title="test1", completed=False)
         self.task2 = mommy.prepare(Task, title="test2", completed=True)
 
     def test_task_list_view(self) -> None:
-        response = self.client.get("/api/task-list/")
+        url = "/api/task-list/"
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_task_list(self) -> None:
