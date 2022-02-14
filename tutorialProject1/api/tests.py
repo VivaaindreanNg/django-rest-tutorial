8f9c12555e9648f4b8516395797b46e82cb1a6c3
VivaaindreanNg <123ulugubang@gmail.com>
2022-02-14 19:39:44 +0800

    Added test cases for Detail viewset.

--- tutorialProject1/api/tests.py
+++ tutorialProject1/api/tests.py
@@ -9,10 +9,10 @@ class TestView(APITestCase):
     def setUp(self) -> None:
         self.task1 = mommy.make(Task, title="test1", completed=False)
         self.task2 = mommy.make(Task, title="test2", completed=True)
+        self.list_all_url = "/api/task-list/"
 
     def test_task_list_view(self) -> None:
-        url = "/api/task-list/"
-        response = self.client.get(url)
+        response = self.client.get(self.list_all_url)
         response_data = response.json()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -21,8 +21,15 @@ class TestView(APITestCase):
         self.assertEqual(response_data[1]["title"], "test2")
         self.assertTrue(response_data[1]["completed"])
 
-    # def test_task_detail_view(self) -> None:
-    #     url = "/api/task-detail/"
-    #     import pdb
+    def test_task_detail_view(self) -> None:
+        response = self.client.get(self.list_all_url)
+        response_data = response.json()
+        id_0 = response_data[0]["id"]
 
-    #     # pdb.set_trace()
+        detail_url = f"/api/task-detail/{id_0}/"
+        get_response = self.client.get(detail_url)
+        get_response_data = get_response.json()
+
+        self.assertEqual(get_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(get_response_data["title"], "test1")
+        self.assertFalse(get_response_data["completed"])
