d8247d20b2e3d55e4408226ddf8621b5f4773e90
VivaaindreanNg <123ulugubang@gmail.com>
2022-03-19 22:28:55 +0800

    Updated models.py

--- tutorialProject1/api/models.py
+++ tutorialProject1/api/models.py
@@ -11,8 +11,16 @@ class Task(models.Model):
         (NON_URGENT, "Non-Urgent"),
     )
     title = models.CharField(max_length=100)
-    completed = models.BooleanField(default=False, blank=True, null=True)
-    types = models.CharField(max_length=15, choices=GENRE, default=NON_URGENT)
+    completed = models.BooleanField(
+        default=False,
+        blank=True,
+        null=True,
+    )
+    types = models.CharField(
+        max_length=15,
+        choices=GENRE,
+        default=NON_URGENT,
+    )
 
     def __str__(self):
         return self.title
