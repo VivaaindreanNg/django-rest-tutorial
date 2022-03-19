de2b014b005e2828c8be18294803d07520a49f6a
VivaaindreanNg <123ulugubang@gmail.com>
2022-03-19 22:16:20 +0800

    Updated models.py

--- tutorialProject1/api/models.py
+++ tutorialProject1/api/models.py
@@ -4,8 +4,15 @@ from django.db import models
 
 
 class Task(models.Model):
+    URGENT = "u"
+    NON_URGENT = "nu"
+    GENRE = (
+        (URGENT, "Urgent"),
+        (NON_URGENT, "Non-Urgent"),
+    )
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False, blank=True, null=True)
+    types = models.CharField(max_length=15, choices=GENRE, default=NON_URGENT)
 
     def __str__(self):
         return self.title
